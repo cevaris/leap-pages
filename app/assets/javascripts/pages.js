f671cc28592fc3edc560605515bbdf71a0f334f3
Cevaris <cevaris@gmail.com>
2014-02-17 16:37:56 -0700

    got page swipe to work

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -20,6 +20,10 @@
 
   };
 
+  pageNumber = function(){
+    return pageNum;
+  }
+
   observeKeypress = function(){
     $(document).on('keyup', function(e){
       if (e.keyCode == RIGHT) { 
@@ -35,21 +39,34 @@
     });
   };
 
+  blockInput = function(time){
+    // Create hidden field
+    $('<input>').attr({type: 'hidden',id: 'blockInput',name: 'blockInput'}).appendTo(selector);
+    // Delete hidden field after N time
+    setTimeout(function(){ $('#blockInput').remove() }, time);
+  }
+
   render = function(){
     // console.log(pageData);
-    console.log(pageNum);
-    console.log(pageData.pages[pageNum].content);
+    // console.log(pageNum);
+    // console.log(pageData.pages[pageNum].content);
+
     selector.html(pageData.pages[pageNum].content);
+    blockInput(1000);
   };
 
-  prevPage = function(){
+  $.fn.prevPage = function(){
+    if($('#blockInput').length) return false;
+
     if(pageNum > 0) pageNum--;
     render();
   };
 
-  nextPage = function(){
+  $.fn.nextPage = function(){
+    if($('#blockInput').length) return false;
+
     if(pageData.pages.length){
-      if(pageNum < pageData.pages.length) pageNum++;
+      if(pageNum < pageData.pages.length-1) pageNum++;
       render();
     }
   };
@@ -67,9 +84,6 @@
     if(config.sourceUrl) {
       fetch(config.sourceUrl);
     }
-
-    
-
   };
 
 }( jQuery ));
