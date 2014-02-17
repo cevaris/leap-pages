3234949f0ed7f3ef297eb68468b0777f2923fb96
Cevaris <cevaris@gmail.com>
2014-02-17 16:05:58 -0700

    got arrow navigation working; working on getting gestures

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -7,6 +7,8 @@
   pageNum = 0,
   selector = undefined;
 
+  var LEFT=37, UP=38, RIGHT=39, DOWN=40;
+
 
   $.fn.pages = function(options) {
 
@@ -18,38 +20,56 @@
 
   };
 
+  observeKeypress = function(){
+    $(document).on('keyup', function(e){
+      if (e.keyCode == RIGHT) { 
+        nextPage();
+        console.log("RIGHT");
+        return false;
+      }
+      if (e.keyCode == LEFT) { 
+        console.log("LEFT");
+        prevPage();
+        return false;
+      }
+    });
+  };
+
   render = function(){
     // console.log(pageData);
-    // console.log(pageNum);
-    // console.log(pageData.pages[pageNum].content);
+    console.log(pageNum);
+    console.log(pageData.pages[pageNum].content);
     selector.html(pageData.pages[pageNum].content);
-  }
+  };
 
   prevPage = function(){
     if(pageNum > 0) pageNum--;
     render();
-  }
+  };
 
   nextPage = function(){
-    if(pageData.pages.size){
-      if(pageNum < pageData.pages.size) pageNum++;
+    if(pageData.pages.length){
+      if(pageNum < pageData.pages.length) pageNum++;
       render();
     }
-  }
+  };
 
   fetch = function(sourceUrl) {
     $.getJSON(sourceUrl, function(json){
       pageData = json;
       render();
     });
-  }
+  };
 
   init = function() {
+    observeKeypress();
+
     if(config.sourceUrl) {
       fetch(config.sourceUrl);
     }
 
     
+
   };
 
 }( jQuery ));
